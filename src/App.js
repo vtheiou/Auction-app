7980642f8031b2b5d7f2a962fde2bb93a97ac3cb
Your Name <theiou@uth.gr>
2021-06-01 14:32:40 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import AuctionContract from "./contracts/Auction.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = {contractBalance:0,userBalances:0,highestBidder:null,highestBid: 0, web3: null, accounts: null, contract: null,current_bid:"" };
 
   componentDidMount = async () => {
     try {
-      // Get network provider and web3 instance.
+      
       const web3 = await getWeb3();
 
-      // Use web3 to get the user's accounts.
+      
       const accounts = await web3.eth.getAccounts();
 
-      // Get the contract instance.
+      
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const deployedNetwork = AuctionContract.networks[networkId];
       const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
+        AuctionContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
+    const current_bid = "";
+    const contractBalance = await instance.methods.getContractBalance().call();
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+    const highest_bid = await instance.methods.getHighestBid().call();
+    const highest_bid_addr = await instance.methods.getHighestBidderAddress().call();
+    
+      this.setState({ web3, accounts, contract: instance ,highestBid:highest_bid,highestBidder:highest_bid_addr,getContractBalance:contractBalance,current_bid:""});
     } catch (error) {
-      // Catch any errors for any of the above operations.
       alert(
         `Failed to load web3, accounts, or contract. Check console for details.`,
       );
@@ -35,36 +37,87 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
+  bid_1 = async () => {
+    const { accounts, contract,current_bid} = this.state;
+    const bid_value = this.state.current_bid;
+    const withdraw1 = await contract.methods.bid().send({ from: accounts[0],value:bid_value});
 
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
+    const highest_bid = await contract.methods.getHighestBid().call(); 
+    const highest_bid_addr = await contract.methods.getHighestBidderAddress().call(); 
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
+    this.setState({ highestBid: highest_bid ,highestBidder:highest_bid_addr,userBalances:withdraw1});
+      
   };
+  bid_2 = async () => {
+    const { accounts, contract,current_bid} = this.state;
+    const bid_value = this.state.current_bid;
+    const withdraw1 = await contract.methods.bid().send({ from: accounts[0],value:value});
+    
+    const highest_bid = await contract.methods.getHighestBid().call();
 
+    
+    const highest_bid_addr = await contract.methods.getHighestBidderAddress().call();
+    
+    this.setState({ highestBid: highest_bid ,highestBidder:highest_bid_addr,userBalances:withdraw1});
+      
+  };
+  highestBid = async ()=>{
+    const {accounts,contract} = this.state;
+
+    const highestbid = await contract.methods.getHighestBid().call();
+  }
+  highestBidder = async () => {
+    const {accounts,contract} = this.state;
+    await contract.methods.getHighestBidderAddress().call();
+    console.log(this.state);
+
+  }
+  withdraw = async () => {
+     const {accounts,contract} = this.state;
+     
+     const contractBalance = await contract.methods.getContractBalance().call({from: accounts[0]});
+
+
+     const withdraw1 = await contract.methods.withdraw().send({from:accounts[0]});
+     
+      
+     console.log(this.state);
+     this.setState({getContractBalance:contractBalance,userBalances:withdraw1});
+  }
+
+  getContractBalance = async () =>{
+    const {accounts,contract} = this.state;
+    const contractBalance = await contract.methods.getContractBalance().call({from: accounts[0]});
+    console.log(this.state.contractBalance);
+    this.setState({getContractBalance:contractBalance});
+
+  }
+  myChangeHandler  = (event) => {
+    this.setState({current_bid : event.target.value*1000000000000000000},() =>{
+      console.log(this.state.current_bid);
+    });
+  }
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>Auction Dapp</h1>
+        <div>Enter bid amount!</div>
+        <input type="text"  onChange={this.myChangeHandler}/>
+        <div>Enter for first bidder</div>
+        <div>< button onClick={this.bid_1}> Bidder 1</button></div>
+        <div>Enter for second bidder</div>
+        <div><button onClick={this.bid_2}> Bidder 2</button></div>
+        <div><button onClick={this.highestBid}>Get highestBid</button></div>
+        <div>{this.state.highestBid}></div>
+        <button onClick={this.highestBidder}>Get highestBidderAddress</button>
+        <div>{this.state.highestBidder}></div>
+        <button onClick={this.withdraw}>Withdraw</button>
+        <div>{this.state.withdraw}</div>
+        <button onClick={this.getContractBalance}>Contract Balance</button>
+        <div>{this.state.getContractBalance}</div>
       </div>
     );
   }
